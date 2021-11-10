907601a5be56314a3b133a284aba2ecba4f8b2f2
cicero884 <joy861225@gmail.com>
2021-11-10 08:54:04 +0800

    arrange mux

--- hw2/P76104833/src/AXI/Mux_1_to_N.sv
+++ hw2/P76104833/src/AXI/Mux_1_to_N.sv
@@ -1,8 +1,10 @@
 `include "AXI_define.svh"
 //interface sucks
 //you cant direct assign interface into another interface
-module MUX_1_to_N #(SIZE=1)
-(input [SIZE-1:0]in,input OutPointer cond,output [SIZE-1:0]out1,output [SIZE-1:0]out2,output [SIZE-1:0]out_default)
+module MUX_1_to_N #(SIZE=1)(
+	input [SIZE-1:0]in,input OutPointer cond,
+	output [SIZE-1:0]out1,output [SIZE-1:0]out2,output [SIZE-1:0]out_default
+)
 always_comb begin
 	out1=0;
 	out2=0;
--- hw2/P76104833/src/AXI/Mux_N_to_1.sv
+++ hw2/P76104833/src/AXI/Mux_N_to_1.sv
@@ -1,10 +1,10 @@
 `include "AXI_define.svh"
 //interface sucks
 //you cant direct assign interface into another interface
-module MUX_N_to_1 (#SIZE=1)
-(input [SIZE-1:0]in1,input [SIZE-1:0]in2,input [SIZE-1:0]in_default
-,output OutPointer cond,output [SIZE-1:0]out)
-COND,OUTPUT)
+module MUX_N_to_1 (#SIZE=1)(
+	input [SIZE-1:0]in1,input [SIZE-1:0]in2,input [SIZE-1:0]in_default,
+	output OutPointer cond,output [SIZE-1:0]out
+)
 always_comb begin
 	case(cond):
 		IN1:	out=in1;
