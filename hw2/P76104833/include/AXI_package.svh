9999400cd4beb5dd754ecbfc6683e9954317fa09
cicero884 <joy861225@gmail.com>
2021-12-03 09:20:48 +0800

    tmp save

--- hw2/P76104833/include/AXI_package.svh
+++ hw2/P76104833/include/AXI_package.svh
@@ -91,21 +91,22 @@
 
 
 	// package channels
-	//READ ADDRESS0	=ID +`AddrInfo +`HandShake
-	`define AR_in(NAME)	{ARID_``NAME``,ARADDR_``NAME``,ARLEN_``NAME``,ARSIZE_``NAME``,ARBUST_``NAME``,ARVALID_``NAME``}
+	// READ ADDRESS0	=ID +`AddrInfo +`HandShake
+	`define AR_in(NAME)	{ARID_``NAME``[`AXI_IDM_BITS-1:0],ARADDR_``NAME``,ARLEN_``NAME``,ARSIZE_``NAME``,ARBUST_``NAME``,ARVALID_``NAME``}
 	`define AR_out(NAME) {ARREADY_``NAME``}
 
-	//READ DATA0	=ID +`DataInfo +RESP +`HandShake
-	`define R_in(NAME) {RID_``NAME``,RDATA_``NAME``,RRESP_``NAME``,RLAST_``NAME``,RVALID_``NAME``}
+	// READ DATA0	=ID +`DataInfo +RESP +`HandShake
+	`define R_in(NAME) {RID_``NAME``[`AXI_IDM_BITS-1:0],RDATA_``NAME``,RRESP_``NAME``,RLAST_``NAME``,RVALID_``NAME``}
 	`define R_out(NAME) {RREADY_``NAME``}
 
-	//WRITE ADDRESS	=ID +`AddrInfo +`HandShake
-	//WRITE DATA	=   +`DataInfo +WSTRB +`HandShake
-	`define W_in(NAME) {AWID_``NAME``,AWADDR_``NAME``,AWLEN_``NAME``,AWSIZE_``NAME``,AWBURST_``NAME``,AWVALID_``NAME``,WDATA_``NAME``,WSTRB_``NAME``,WLAST_``NAME``,WVALID_``NAME``}
+	// WRITE ADDRESS	=ID +`AddrInfo +`HandShake
+	// WRITE DATA	=   +`DataInfo +WSTRB +`HandShake
+	`define W_in(NAME) {AWID_``NAME``[`AXI_IDM_BITS-1:0],AWADDR_``NAME``,AWLEN_``NAME``,AWSIZE_``NAME``,AWBURST_``NAME``,AWVALID_``NAME``,WDATA_``NAME``,WSTRB_``NAME``,WLAST_``NAME``,WVALID_``NAME``}
 	`define W_out(NAME) {AWREADY_``NAME``,WREADY_``NAME``}
 
-	//WRITE RESPONSE=ID +RESP +`Handshake
-	`define B_in(NAME) {BID_``NAME``,BRESP_``NAME``,BVALID_``NAME``}
+	// WRITE RESPONSE=ID +RESP +`Handshake
+	`define B_in(NAME) {BID_``NAME``[`AXI_IDM_BITS-1:0],BRESP_``NAME``,BVALID_``NAME``}
 	`define B_out(NAME) {BREADY_``NAME``}
 
+	// connect wires
 `endif
