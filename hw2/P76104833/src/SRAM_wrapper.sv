d8418686aa35369d8476258127566eda27e83a68
cicero884 <joy861225@gmail.com>
2021-12-05 13:15:06 +0800

    Fix SRAM_wrapper(TODO default slave,master wrapper)

--- hw2/P76104833/src/SRAM_wrapper.sv
+++ hw2/P76104833/src/SRAM_wrapper.sv
@@ -2,6 +2,7 @@
 `include "AXI_package.svh"
 `include "AXI_wrapper.svh"
 
+// TODO: change state machine into pipeline?
 module SRAM_wrapper(
 	input clk,input rst,
 	//READ ADDRESS
@@ -59,7 +60,8 @@ logic [1:0] ARBURST;
 
 Responce resp;
 assign resp=OKAY;
-assign RRESP=resp;
+assign RRESP_S=resp;
+assign RDATA_S=DO;
 
 always_ff @(posedge clk,posedge rst) begin
 	if(rst) begin
@@ -71,8 +73,7 @@ always_ff @(posedge clk,posedge rst) begin
 	else begin
 		case(read_state)
 			IDLE:begin
-				RLAST_S<=1'b0;
-				RVALID_S<=1'b0;
+				// set AR
 				if(ARVALID_S) begin
 					read_state<=TRANSMITTING;
 					RID_S<=ARID_S;
@@ -83,38 +84,35 @@ always_ff @(posedge clk,posedge rst) begin
 					ARREADY_S<=1'b0;
 				end
 				else ARREADY_S<=1'b1;// default high(view spec)
+				// clean R
+				RLAST_S<=1'b0;
+				RVALID_S<=1'b0;
 			end
 			TRANSMITTING: begin
-				if(RREADY_S) begin
-					if(write_state!=TRANSMITTING) begin
-						//assume INCR type
+				// set R
+				// assume INCR
+				if(write_state==TRANSMITTING) RVALID_S<=1'b0;
+				else begin
+					RVALID_S<=1'b1;
+					if(RREADY_S) begin
+						ARLEN<=ARLEN-1;
+						ARADDR<=ARADDR+4;
+						// R end
 						if(ARLEN==0) begin
 							RLAST_S<=1'b1;
 							read_state<=IDLE;
 						end
-						RDATA_S<=DO;
-						ARLEN<=ARLEN-1;
-						ARADDR<=ARADDR+4;
-						RVALID_S<=1'b1;
 					end
-					else RVALID_S<=1'b0;
 				end
 			end
 		endcase
 	end
 end
-always_latch begin
-	if(rst) begin
-	end
-	else begin
-		case(read_state)
-end
 
 // Write handler
 //save write AddrInfo in AW_S,cast on aw to use
 //AddrInfo aw;
-assign BRESP=resp;
-//logic [`AXI_LEN_BITS-1:0]len_cnt_w;
+assign BRESP_S=resp;
 logic [`AXI_ADDR_BITS-1:0] AWADDR;
 logic [`AXI_LEN_BITS-1:0] AWLEN;
 logic [`AXI_SIZE_BITS-1:0] AWSIZE;
@@ -125,6 +123,7 @@ always_ff @(posedge clk,posedge rst) begin
 		write_state<=IDLE;
 		AWREADY_S<=1'b1;// default high(view spec)
 		WREADY_S<=1'b0;
+		WEB<=`AXI_STRB_BITS'd0;
 		BVALID_S<=1'b0;
 	end
 	else begin
@@ -159,105 +158,107 @@ always_ff @(posedge clk,posedge rst) begin
 			end
 			BACK:begin
 				BVALID_S<=1'b1;
+				WEB<=`AXI_STRB_BITS'd0;
+				WREADY_S<=1'b0;
 				if(BREADY_S) begin
 					if(AWVALID_S&&read_state!=TRANSMITTING)
-					write_state<=IDLE;
-			end
-		endcase
+						write_state<=IDLE;
+				end
+			endcase
+		end
 	end
-end
 
-assign A=(write_state==TRANSMITTING)? aw.addr[15:2]:ar.addr[15:2];
-assign DI=W_S.data;
-assign CS=(write_state==TRANSMITTING | read_state==TRANSMITTING);
-assign OE=(read_state==TRANSMITTING);
+	assign A=(write_state==TRANSMITTING)? AWADDR[15:2]:ARADDR[15:2];
+	assign DI=W_S.data;
+	assign CS=(write_state==TRANSMITTING | read_state==TRANSMITTING);
+	assign OE=(read_state==TRANSMITTING);
 
-SRAM i_SRAM (
-	.A0   (A[0]  ),
-	.A1   (A[1]  ),
-	.A2   (A[2]  ),
-	.A3   (A[3]  ),
-	.A4   (A[4]  ),
-	.A5   (A[5]  ),
-	.A6   (A[6]  ),
-	.A7   (A[7]  ),
-	.A8   (A[8]  ),
-	.A9   (A[9]  ),
-	.A10  (A[10] ),
-	.A11  (A[11] ),
-	.A12  (A[12] ),
-	.A13  (A[13] ),
-	.DO0  (DO[0] ),
-	.DO1  (DO[1] ),
-	.DO2  (DO[2] ),
-	.DO3  (DO[3] ),
-	.DO4  (DO[4] ),
-	.DO5  (DO[5] ),
-	.DO6  (DO[6] ),
-	.DO7  (DO[7] ),
-	.DO8  (DO[8] ),
-	.DO9  (DO[9] ),
-	.DO10 (DO[10]),
-	.DO11 (DO[11]),
-	.DO12 (DO[12]),
-	.DO13 (DO[13]),
-	.DO14 (DO[14]),
-	.DO15 (DO[15]),
-	.DO16 (DO[16]),
-	.DO17 (DO[17]),
-	.DO18 (DO[18]),
-	.DO19 (DO[19]),
-	.DO20 (DO[20]),
-	.DO21 (DO[21]),
-	.DO22 (DO[22]),
-	.DO23 (DO[23]),
-	.DO24 (DO[24]),
-	.DO25 (DO[25]),
-	.DO26 (DO[26]),
-	.DO27 (DO[27]),
-	.DO28 (DO[28]),
-	.DO29 (DO[29]),
-	.DO30 (DO[30]),
-	.DO31 (DO[31]),
-	.DI0  (DI[0] ),
-	.DI1  (DI[1] ),
-	.DI2  (DI[2] ),
-	.DI3  (DI[3] ),
-	.DI4  (DI[4] ),
-	.DI5  (DI[5] ),
-	.DI6  (DI[6] ),
-	.DI7  (DI[7] ),
-	.DI8  (DI[8] ),
-	.DI9  (DI[9] ),
-	.DI10 (DI[10]),
-	.DI11 (DI[11]),
-	.DI12 (DI[12]),
-	.DI13 (DI[13]),
-	.DI14 (DI[14]),
-	.DI15 (DI[15]),
-	.DI16 (DI[16]),
-	.DI17 (DI[17]),
-	.DI18 (DI[18]),
-	.DI19 (DI[19]),
-	.DI20 (DI[20]),
-	.DI21 (DI[21]),
-	.DI22 (DI[22]),
-	.DI23 (DI[23]),
-	.DI24 (DI[24]),
-	.DI25 (DI[25]),
-	.DI26 (DI[26]),
-	.DI27 (DI[27]),
-	.DI28 (DI[28]),
-	.DI29 (DI[29]),
-	.DI30 (DI[30]),
-	.DI31 (DI[31]),
-	.CK   (clk   ),
-	.WEB0 (WEB[0]),
-	.WEB1 (WEB[1]),
-	.WEB2 (WEB[2]),
-	.WEB3 (WEB[3]),
-	.OE   (OE    ),
-	.CS   (CS    )
-);
+	SRAM i_SRAM (
+		.A0   (A[0]  ),
+		.A1   (A[1]  ),
+		.A2   (A[2]  ),
+		.A3   (A[3]  ),
+		.A4   (A[4]  ),
+		.A5   (A[5]  ),
+		.A6   (A[6]  ),
+		.A7   (A[7]  ),
+		.A8   (A[8]  ),
+		.A9   (A[9]  ),
+		.A10  (A[10] ),
+		.A11  (A[11] ),
+		.A12  (A[12] ),
+		.A13  (A[13] ),
+		.DO0  (DO[0] ),
+		.DO1  (DO[1] ),
+		.DO2  (DO[2] ),
+		.DO3  (DO[3] ),
+		.DO4  (DO[4] ),
+		.DO5  (DO[5] ),
+		.DO6  (DO[6] ),
+		.DO7  (DO[7] ),
+		.DO8  (DO[8] ),
+		.DO9  (DO[9] ),
+		.DO10 (DO[10]),
+		.DO11 (DO[11]),
+		.DO12 (DO[12]),
+		.DO13 (DO[13]),
+		.DO14 (DO[14]),
+		.DO15 (DO[15]),
+		.DO16 (DO[16]),
+		.DO17 (DO[17]),
+		.DO18 (DO[18]),
+		.DO19 (DO[19]),
+		.DO20 (DO[20]),
+		.DO21 (DO[21]),
+		.DO22 (DO[22]),
+		.DO23 (DO[23]),
+		.DO24 (DO[24]),
+		.DO25 (DO[25]),
+		.DO26 (DO[26]),
+		.DO27 (DO[27]),
+		.DO28 (DO[28]),
+		.DO29 (DO[29]),
+		.DO30 (DO[30]),
+		.DO31 (DO[31]),
+		.DI0  (DI[0] ),
+		.DI1  (DI[1] ),
+		.DI2  (DI[2] ),
+		.DI3  (DI[3] ),
+		.DI4  (DI[4] ),
+		.DI5  (DI[5] ),
+		.DI6  (DI[6] ),
+		.DI7  (DI[7] ),
+		.DI8  (DI[8] ),
+		.DI9  (DI[9] ),
+		.DI10 (DI[10]),
+		.DI11 (DI[11]),
+		.DI12 (DI[12]),
+		.DI13 (DI[13]),
+		.DI14 (DI[14]),
+		.DI15 (DI[15]),
+		.DI16 (DI[16]),
+		.DI17 (DI[17]),
+		.DI18 (DI[18]),
+		.DI19 (DI[19]),
+		.DI20 (DI[20]),
+		.DI21 (DI[21]),
+		.DI22 (DI[22]),
+		.DI23 (DI[23]),
+		.DI24 (DI[24]),
+		.DI25 (DI[25]),
+		.DI26 (DI[26]),
+		.DI27 (DI[27]),
+		.DI28 (DI[28]),
+		.DI29 (DI[29]),
+		.DI30 (DI[30]),
+		.DI31 (DI[31]),
+		.CK   (clk   ),
+		.WEB0 (WEB[0]),
+		.WEB1 (WEB[1]),
+		.WEB2 (WEB[2]),
+		.WEB3 (WEB[3]),
+		.OE   (OE    ),
+		.CS   (CS    )
+	);
 
-endmodule
+	endmodule
